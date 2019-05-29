6794bff705acc898209ffb1b7e798e33ae924485
Deepak Gupta <raftaar1191@gmail.com>
2019-05-29 12:46:36 +0530

    Adding plugin index #1

---
+++ wordpress-default-data.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WordPress Default Data
+ * Description: Create lots of users, comment, posts, pages, groups, activity items, messages, profile data - useful for WordPress, BuddyPress and bbPress testing purposes.
+ * Author:      raftaar1191
+ * Version:     1.0.0
+ * Text Domain: wp-default-data
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
